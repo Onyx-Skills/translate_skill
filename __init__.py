05df207c85685f8f11d898bbdf8861163d6f8ab2
aituglo <aituglo@localhost.localdomain>
2019-11-03 08:29:01 +0000

    add vocab

--- __init__.py
+++ __init__.py
@@ -1,8 +1,11 @@
 from os.path import dirname, join
+from adapt.intent import IntentBuilder
 
-from translate_skill.index import translate
+from translate_skill.index import translate as blueprint
 from onyx.skills.core import OnyxSkill
 from onyx.util.log import getLogger
+
+from mtranslate import translate
 
 __author__ = ''
 
@@ -13,10 +16,52 @@ class TranslateSkill(OnyxSkill):
         super(TranslateSkill, self).__init__(name="TranslateSkill")
 
     def get_blueprint(self):
-        return translate
+        return blueprint
 
     def initialize(self):
         LOGGER.info("Translate Skill initialize")
+
+        self.load_data_files(dirname(__file__))
+        self.load_regex_files(join(dirname(__file__), 'regex', self.lang))
+
+
+        intent = IntentBuilder('TranslateIntent')\
+            .require('TranslateKeyword') \
+            .require('LanguageKeyword') \
+            .require('phrase') \
+            .build()
+        self.register_intent(intent, self.handle_translate)
+
+        intent = IntentBuilder('TranslateToIntent')\
+            .require('TranslateKeyword') \
+            .require('translate') \
+            .require('ToKeyword') \
+            .require('LanguageKeyword') \
+            .build()
+        self.register_intent(intent, self.handle_translate_to)
+
+    def handle_translate(self, message):
+    	word = message.data.get("TranslateKeyword")
+    	lang = message.data.get("LanguageKeyword")
+    	sentence = message.data.get("phrase")
+
+    	translated = translate(sentence, lang)
+
+    	self.say(translated,lang)
+
+    def handle_translate_to(self, message):
+    	lang = message.data.get("LanguageKeyword")
+    	sentence = message.data.get("translate")
+    	to = message.data.get("ToKeyword")
+
+    	translated = translate(sentence, lang)
+
+    	self.say(translated,lang)
+
+
+    def say(self,sentence,lang):
+    	print("TRANSLATED PHRASE:",sentence )
+    	self.speak(sentence, lang)
 
     def stop(self):
         pass
